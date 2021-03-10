5b06656998c1ee487ba89206f1998c5e69cda36a
giorgia <giorgia@192.168.1.51>
2021-03-10 12:51:23 +0100

    Soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -21,6 +21,80 @@ public class Libretto {
 		
 	}
 	
+	/*
+	 
+	 
+	public void stampaVotiUguali(int punteggio) {
+		//il libretto stampa da solo i voti
+		//questo metodo sa solo stampare nella console
+		System.out.println();
+	}
+	
+	*/
+	
+	//oppure
+	
+	/*
+	public String votiUguali(int punteggio) {
+		//calcola una stringa che contiene i voti
+		//sara' poi il chiamante a stamparli
+		//questo metodo invece crea una stringa e poi sono io a decidere come usarla
+		//questo metodo e' piu' generale e quindi migliore del primo
+		//ma nella stringa devo stampare solo il nome del corso oppure tutto voto.toString() ??
+		//non ho informazioni chiare dal testo per decidere come comporre questa stringa
+		//quindi seleziono (con il metodo successivo) i voti cercati, e poi uno se li stampa come vuole
+	}
+	
+	*/
+	
+	
+	
+	public List<Voto> listaVotiUguali(int punteggio) {
+		
+		List<Voto> risultato = new ArrayList<>();
+		
+		for(Voto v : this.voti) {
+			
+			if(v.getVoto()==punteggio) {
+				
+				risultato.add(v);
+				
+				//v ora e' lo STESSO OGGETTO VOTO contenuto in DUE LISTE DIVERSE, non sono due oggetti diversi copiati, ma sono proprio lo STESSO OGGETTO, perche' non abbiamo scritto new creando un nuovo oggetto, ma stiamo usando un oggetto che esiste gia'
+			}
+			
+		}
+		
+		return risultato;
+		
+		//restituisce solo i voti uguali al criterio
+		//ha un difetto = espone all'esterno il tipo di struttura dati interna che ho usato
+		//se poi decido di organizzare i dati in modo diverso, dovrei modificare tutto
+		
+	}
+	
+	
+	
+	//alternativa = METODO MIGLIORE
+	
+	public Libretto votiUguali(int punteggio) {
+		
+		//restituisco un nuovo libretto con i voti cercati
+		//in questo modo, in questo nuovo libretto ci sono tutte le operazioni che si avevano nel libretto completo iniziale
+		
+		Libretto risultato = new Libretto();
+		
+		for(Voto v : this.voti) {
+			
+			if(v.getVoto()==punteggio) {
+				risultato.add(v);
+				//risultato.voti.add(v);
+			}
+			
+		}
+		
+		return risultato;
+	}
+	
 	public String toString() {
 		
 		String s = "";
